3643040188ace5e61a1c2be6e0738e00f819ba57
jcodestechnology <jcodes2023@gmail.com>
2024-04-02 15:41:57 +0300

    Updated installation script to include Zeek and Rita installation

--- install.sh
+++ install.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# Install tcpdump
+# Update apt-get
 sudo apt-get update
+
+# Install tcpdump
 sudo apt-get install -y tcpdump
 
 # Set permissions to allow non-root users to capture packets
 sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
+
+# Install Zeek (formerly known as Bro)
+sudo apt-get install -y zeek
+
+# Install Rita
+git clone https://github.com/activecm/rita.git
+cd rita
+sudo python3 setup.py install
+cd ..
 
 # Inform user about completion
 echo "Installation complete!"
